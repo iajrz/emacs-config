c3ea8e012ac0f07adb1222a2d95bd787b0b82717
Irving Rivas <axel.rivas@gmail.com>
2016-06-11 00:01:26 -0400

    add visible bell; remove auto-save-list

--- init.el
+++ init.el
@@ -12,7 +12,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("40f6a7af0dfad67c0d4df2a1dd86175436d79fc69ea61614d668a635c2cd94ab" default)))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
